cac055ed195c4aabbf7c6ebd120c81584e2db490
Sandra <sandra.grujovic@tum.de>
2018-05-14 21:20:42 +0200

    set up project

---
+++ pixelino/pixelino/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pixelino
+//
+//  Created by Sandra Grujovic on 14.05.18.
+//  Copyright © 2018 Sandra Grujovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
