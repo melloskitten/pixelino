5fdb0dc1a9f9a56b6b6cab72ccd7f6ed217d0b84
Sandra <sandra.grujovic@tum.de>
2018-05-23 21:22:04 +0200

    initial screen rotation implementation; refactor the viewDidLoad() method
    Co-authored-by: Johannes Rohwer <mail@johannesrohwer.com>

--- pixelino/pixelino/ViewController.swift
+++ pixelino/pixelino/ViewController.swift
@@ -11,17 +11,91 @@ import SpriteKit
 
 let darkGrey = UIColor(red:0.10, green:0.10, blue:0.10, alpha:1.0)
 let lightGrey = UIColor(red:0.19, green:0.19, blue:0.19, alpha:1.0)
+let PIXEL_SIZE = 200
+// FIXME: make this dynamic
+let SCREEN_HEIGHT = 2436
+let SCREEN_WIDTH = 1125
+
 
 class Pixel : SKShapeNode {
+    init(x: Int, y: Int) {
+        super.init()
 
+        self.position = CGPoint(x: x * PIXEL_SIZE, y: y * PIXEL_SIZE)
+        self.fillColor = .white
+        self.strokeColor = UIColor.gray
+        self.lineWidth = 3
+        
+        self.path = UIBezierPath(rect: CGRect(x: 0, y: 0, width: PIXEL_SIZE, height: PIXEL_SIZE)).cgPath
+        self.isUserInteractionEnabled = true
+        self.isAntialiased = false
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+class Canvas : SKShapeNode {
+    
+    let grid = [[Pixel]]()
+    
+    init(width: Int, height: Int) {
+        super.init()
+        
+        for x in 0..<width {
+            for y in 0..<height {
+                let pixel = Pixel(x: x, y: y)
+                //grid[x][y] = s
+                self.addChild(pixel)
+            }
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class CanvasView : SKView {
+    
+    var canvasScene : SKScene
+    var camera : SKCameraNode
+    
+    init() {
+        
+        self.canvasScene = SKScene(size: CGSize(width: SCREEN_WIDTH, height: SCREEN_HEIGHT))
+
+        self.canvasScene.backgroundColor = UIColor(red:0.10, green:0.10, blue:0.10, alpha:1.0)
+        self.canvasScene.isUserInteractionEnabled = true
+        self.camera = SKCameraNode()
+        self.camera.position = CGPoint(x: 100, y: 100)
+        
+        super.init(frame: CGRect(x: 0, y: 0, width: SCREEN_WIDTH, height: SCREEN_HEIGHT))
+
+        
+        self.canvasScene.addChild(camera)
+        self.canvasScene.camera = camera
+        let canvas = Canvas(width: 10, height: 10)
+        self.canvasScene.addChild(canvas)
+        self.canvasScene.scaleMode = .aspectFill
+        
+        presentScene(self.canvasScene)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 
 class ViewController: UIViewController {
-
-    var skView : SKView? = nil
-    var skScene : SKScene? = nil
+    
+    var canvasView : SKView? = nil
+    var canvasScene : SKScene? = nil
+    var toolbarView : UIView? = nil
+    
     
     override var prefersStatusBarHidden: Bool {
         get {
@@ -31,63 +105,39 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let SCREEN_HEIGHT = 2436
-        let SCREEN_WIDTH = 1125
-        let cameraNode = SKCameraNode()
        
-        self.view = SKView(frame: CGRect(x: 0, y: 0, width: SCREEN_WIDTH, height: SCREEN_HEIGHT))
-        skView = self.view as? SKView
-        skView?.showsFPS = true
-        skScene = SKScene(size: CGSize(width: SCREEN_WIDTH, height: SCREEN_HEIGHT))
-       
+        // Setup scene and view
+        self.canvasView = CanvasView()
+        self.canvasScene = canvasView?.scene
+        self.view = canvasView
         
-        skScene?.backgroundColor = UIColor(red:0.10, green:0.10, blue:0.10, alpha:1.0)
-        
-        // Generate pixel grid
-        let PIXEL_SIZE = 200
-        for x in 0..<10 {
-            for y in 0..<10 {
-                let s = Pixel(rectOf: CGSize(width: PIXEL_SIZE, height: PIXEL_SIZE))
-                s.fillColor = .white
-                s.isUserInteractionEnabled = true
-                s.strokeColor = UIColor.gray
-                s.lineWidth = 3
-                s.isAntialiased = false
-                s.position = CGPoint(x: x * PIXEL_SIZE, y: y * PIXEL_SIZE)
-                skScene?.addChild(s)
-            }
-        }
-        
-        
-        skScene?.scaleMode = .aspectFill
-
-        cameraNode.position = CGPoint(x: 100, y:100)
-        skScene?.addChild(cameraNode)
-        skScene?.camera = cameraNode
-        
-        // Add navigation gesture handler
+        // Add handlers
         let zoomGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(handlePinchFrom(_:)))
         zoomGestureRecognizer.delegate = self
-      
+        
         let navigatorGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePanFrom(_:)))
         navigatorGestureRecognizer.minimumNumberOfTouches = 2
         navigatorGestureRecognizer.delegate = self
-
+        
         let drawGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handleDrawFrom(_:)))
         drawGestureRecognizer.maximumNumberOfTouches = 1
         
-        //FIXME: Adding for debugging
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTapFrom(_:)))
-    
-        // Add handlers
+        
         view.addGestureRecognizer(zoomGestureRecognizer)
         view.addGestureRecognizer(navigatorGestureRecognizer)
         view.addGestureRecognizer(drawGestureRecognizer)
         view.addGestureRecognizer(tapGestureRecognizer)
         
+        // Set up the tool bar
+        let screenSize = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
         
-        skView?.presentScene(skScene)
-        // Do any additional setup after loading the view, typically from a nib.
+        toolbarView = UIView(frame: CGRect(origin: CGPoint(x: 0, y: screenHeight-100), size: CGSize(width: screenWidth, height: 100 )))
+        toolbarView?.backgroundColor = lightGrey
+        
+        self.view.addSubview(toolbarView!)
     }
     
     @objc func handlePinchFrom(_ sender: UIPinchGestureRecognizer) {
@@ -95,24 +145,59 @@ class ViewController: UIViewController {
         var scale = sender.scale
         let absolute = abs(1 - scale)
         scale = scale < 1 ? 1 + absolute : 1 - absolute
-
+        
         let pinch = SKAction.scale(by: scale, duration: 0.0)
         
         sender.scale = 1.0
-        skScene?.camera?.run(pinch)
-       
+        canvasScene?.camera?.run(pinch)
+        
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        
+        // FIXME: perform nice animation on draw view and no animaton for the menu.
+        
+        let MENU_BAR_HEIGHT : CGFloat = 100.0
+        var subViews = self.view.subviews
+        if subViews.isEmpty {
+            return
+        }
+        
+        // Fetch screen size
+        let screenWidth = size.width
+        let screenHeight = size.height
+        
+        let subView = subViews[0]
+        
+        switch UIDevice.current.orientation {
+        case .landscapeLeft:
+            subView.frame = CGRect(x: screenWidth - MENU_BAR_HEIGHT, y: 0 , width: MENU_BAR_HEIGHT, height: screenHeight)
+        case .landscapeRight:
+            subView.frame = CGRect(x: 0, y: 0 , width: MENU_BAR_HEIGHT, height: screenHeight)
+        case .portrait:
+            subView.frame = CGRect(x: 0, y:screenHeight - MENU_BAR_HEIGHT , width: screenWidth, height: MENU_BAR_HEIGHT)
+        default: break
+        }
+        
+        /*coordinator.animate(alongsideTransition: nil, completion:
+            {_ in
+                UIView.setAnimationsEnabled(true)
+        })
+        
+        UIView.setAnimationsEnabled(false)*/
+        
+        super.viewWillTransition(to: size, with: coordinator)
     }
     
     @objc func handleDrawFrom(_ sender: UIPanGestureRecognizer) {
         let touchLocation = sender.location(in: sender.view)
-        let touchLocationInScene = skView?.convert(touchLocation, to: skScene!)
-
-        let nodes = skScene?.nodes(at: touchLocationInScene!)
+        let touchLocationInScene = canvasView?.convert(touchLocation, to: canvasScene!)
+        
+        let nodes = canvasScene?.nodes(at: touchLocationInScene!)
         
         nodes?.forEach({ (node) in
             if let pixel = node as? Pixel {
                 pixel.fillColor = .blue
-                //pixel.fillColor = pixel.fillColor == .yellow ? .black : .yellow
             }
         })
         
@@ -121,9 +206,9 @@ class ViewController: UIViewController {
     
     @objc func handleTapFrom(_ sender: UITapGestureRecognizer) {
         let touchLocation = sender.location(in: sender.view)
-        let touchLocationInScene = skView?.convert(touchLocation, to: skScene!)
+        let touchLocationInScene = canvasView?.convert(touchLocation, to: canvasScene!)
         
-        let nodes = skScene?.nodes(at: touchLocationInScene!)
+        let nodes = canvasScene?.nodes(at: touchLocationInScene!)
         
         nodes?.forEach({ (node) in
             if let pixel = node as? Pixel {
@@ -133,12 +218,12 @@ class ViewController: UIViewController {
     }
     
     @objc func handlePanFrom(_ sender: UIPanGestureRecognizer) {
-        let translation = sender.translation(in: skView)
+        let translation = sender.translation(in: canvasView)
         let pan = SKAction.moveBy(x: -1.0 * translation.x, y:  translation.y, duration: 0)
-        skScene?.camera?.run(pan)
-        sender.setTranslation(CGPoint.zero, in: skView)
+        canvasScene?.camera?.run(pan)
+        sender.setTranslation(CGPoint.zero, in: canvasView)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -148,7 +233,7 @@ class ViewController: UIViewController {
 extension ViewController: UIGestureRecognizerDelegate {
     
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-            return true
+        return true
     }
 }
 
