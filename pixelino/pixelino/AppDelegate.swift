f4ed5253306f2e2d628344ad74e84156349eb969
Sandra <sandra.grujovic@tum.de>
2018-08-14 22:25:28 +0200

    add new main menu vc + get rid of main.storyboard

--- pixelino/pixelino/AppDelegate.swift
+++ pixelino/pixelino/AppDelegate.swift
@@ -15,9 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let startViewController = MainMenuTableViewController()
+        window?.rootViewController = startViewController
+        window?.makeKeyAndVisible()
         return true
     }
 
