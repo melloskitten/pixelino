a30a15776a49fabd467b94e387ed73b5a850f655
Sandra <sandra.grujovic@tum.de>
2018-08-25 20:40:31 +0200

    merge conflict with static files part 2

--- pixelino/pixelino/AppDelegate.swift
+++ pixelino/pixelino/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        let startViewController = MainMenuTableViewController()
+        let startViewController = ViewController()
         window?.rootViewController = startViewController
         window?.makeKeyAndVisible()
         return true
