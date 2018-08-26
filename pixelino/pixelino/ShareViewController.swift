29c1c7cdd921ea79bb2ceb23a1e7407e7c2dc007
Sandra <sandra.grujovic@tum.de>
2018-08-26 20:37:44 +0200

    introduce new shareviewcontroller, added uiactivityviewcontroller for sharing with other apps and saving to fotos, refactored pictureexporter class and removed dead code

---
+++ pixelino/pixelino/ShareViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ShareViewController.swift
+//  
+//
+//  Created by Sandra Grujovic on 26.08.18.
+//
+
+import UIKit
+
+class ShareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.vie
+    }
+
+}
