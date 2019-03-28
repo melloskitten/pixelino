b181e9b56a11343fa55c3b516a2d33581488c32a
johannesrohwer <mail@johannesrohwer.com>
2019-03-28 14:06:33 +0100

    chore: add hero to podfile dependencies

--- src/pixelino/Pods/Target Support Files/Pods-pixelino/Pods-pixelino-frameworks.sh
+++ src/pixelino/Pods/Target Support Files/Pods-pixelino/Pods-pixelino-frameworks.sh
@@ -143,9 +143,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Hero/Hero.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Pikko/Pikko.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Hero/Hero.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Pikko/Pikko.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
